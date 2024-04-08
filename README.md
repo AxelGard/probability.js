f4893a5aa2385b592a64bc1a7f448c2f7be41712
Axel Gard <axel.gard@protonmail.com>
2024-04-08 21:29:57 +0200

    fixed readme and test

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # probability.js
+
 a extremely small lib for probability in JS (for learning about npm)
+the [npm package](https://www.npmjs.com/package/@axelgard/probability) 
+
+## Install 
+
+```bash
+npm install @axelgard/probability
+```
+
+## Usage
+
+```javascript
+import probability from '@axelgard/probability'
+consol.log(probability(0.5));
+```
--- test/scripts.js
+++ test/scripts.js
@@ -1,6 +1,24 @@
-const probability = require('probability');
+const probability = require('@axelgard/probability')
 
-console.assert(true == prob(0));
-console.assert(false == prob(1));
-console.log("* 0.5: " + probability(0.5))
+function assert(condition, message) {
+    if (!condition) {
+        console.log(message + "\n");
+        console.log("\033[31;1;4mFAILED\033[0m");
+        throw message || "Assertion failed";
+    }
+}
 
+assert(probability(1) === false, "failed on 1")
+assert(probability(1.0) === false, "failed on 1.0")
+assert(probability(0) === true, "failed on 0")
+assert(probability(0.0) === true, "failed on 0.0")
+
+let n = 1_000_000; 
+let check = 0;
+let p = 0.5;
+for (let i = 0; i <= n; i++) {
+   check += probability(p);
+}
+let pression = 0.1;
+assert((check >= n*(p-pression) && check <=  n*(p+pression)),"probablity is not precise enough");
+console.log("\033[32;1;4mPASSED\033[0m")
